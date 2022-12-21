34773c3c24df7a1e42f635da217cd5436ed2c067
Bookantna <67326211+Bookantna@users.noreply.github.com>
2022-12-21 22:46:49 +0700

    Update Okviri.py

--- Okviri.py
+++ Okviri.py
@@ -15,7 +15,10 @@ def main():
                     if len(text)-1 == j:
                         answer += '.\n'
                 elif i == 2:
-                    answer += f'#.{text[j]}.'
+                    if track % 2 == 0 and (track -1) % 3 == 0:
+                        answer += f'.{text[j]}.'
+                    else:
+                        answer += f'#.{text[j]}.'
                     if len(text)-1 == j:
                         answer += '#\n'  
             elif track % 3 == 0:
@@ -31,9 +34,16 @@ def main():
                     answer += f'*.{text[j]}.'
                     if len(text)-1 == j:
                         answer += '*\n' 
-
+                    elif len(text) > j:
+                        answer += '*'
     print(answer,end ='')
-    
+
+
+
+
+
+
+
 if __name__ == '__main__':
     main()
 
